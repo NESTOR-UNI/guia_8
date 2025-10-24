61e389df4535ea702d28cac8f218e173f671bd17
NESTOR-UNI <pipotarea6@gmail.com>
2025-10-23 20:17:01 -0600

    modifocacion de rutina

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Módulo en el que se agrega el método
+  def full_title(page_title = "") [cite: 154]
+    base_title = "Rails_App" [cite: 155]
+    if page_title.empty? [cite: 156]
+      base_title [cite: 157]
+    else
+      page_title + " | " + base_title [cite: 159]
+    end
+  end
 end
